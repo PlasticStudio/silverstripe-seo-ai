0ee82093091edb67d10c1285eaa12a51a0a805a0
Oscar <oscar.holt99@gmail.com>
2024-10-07 15:32:28 +1300

    update form positioning

--- src/Extensions/SeoAIExtension.php
+++ src/Extensions/SeoAIExtension.php
@@ -4,12 +4,16 @@ namespace PlasticStudio\SEOAI\Extensions;
 
 use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\CompositeField;
 use LeKoala\CmsActions\CmsInlineFormAction;
 
 class SeoAIExtension extends DataExtension
 {
     public function updateCMSFields(FieldList $fields)
     {
-        $fields->addFieldToTab('Root.Main', CmsInlineFormAction::create('generateTags', 'Generate SEO Tags'));
+        $fields->insertAfter('SocialImage', CompositeField::create(
+                CmsInlineFormAction::create('generateTags', 'Generate SEO Tags')
+            )->setDescription('Generate SEO tags using AI (this may take several seconds)')
+        );
     }
 }
